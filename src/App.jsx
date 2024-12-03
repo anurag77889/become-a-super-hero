37734619d76ee1da05545fcb5467a783a74f8e80
Anurag Swarnakar <anuragswarnakar626@gmail.com>
2024-12-03 21:06:26 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
   });
 
   const [formErrors, setFormErrors] = useState({});
+  const [preview, setPreview] = useState(null);
 
   // Handle Input Changes
   const handleChange = (e) => {
@@ -34,6 +35,10 @@ function App() {
   const handleFileChange = (e) => {
     const file = e.target.files[0];
     setFormData({ ...formData, profilePicture: file });
+
+    const reader = new FileReader();
+    reader.onloadend = () => setPreview(reader.result);
+    reader.readAsDataURL(file);
   };
 
   // Handle form submission
@@ -43,7 +48,7 @@ function App() {
       console.log("Form Data Submitted: ", formData);
       alert("Congratulations! Marvel or DC may call you anytime soon..");
     } else {
-      alert("You can't be a superhero without being honest.");
+      // alert("You can't be a superhero without being honest.");
     }
   };
 
@@ -73,16 +78,19 @@ function App() {
   };
 
   return (
-    <>
-      <div className="max-w-md mx-auto mt-10 p-6 bg-red-700 rounded-xl shadow-md">
+    <div
+      className="min-h-screen bg-cover bg-center flex items-center justify-center"
+      style={{ backgroundImage: "url('fun.jpg')" }}
+    >
+      <div className="max-w-md ml-56 p-6 bg-black shadow-md">
         <h1 className="text-2xl font-bold text-center mb-6 text-white">
-          !! Register Yourself as a Superhero !!
+          Register Yourself as a Superhero
         </h1>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label
               htmlFor="superheroName"
-              className="block text-white font-semibold pb-2"
+              className="block text-[#00CDFF] font-bold pb-2"
             >
               Superhero Name
             </label>
@@ -100,13 +108,15 @@ function App() {
               }`}
             />
             {formErrors.superheroName && (
-              <p className="text-red-500 text-sm mt-1"></p>
+              <p className="text-[#00CDFF] text-base mt-1">
+                {formErrors.superheroName}
+              </p>
             )}
           </div>
           <div className="mb-4">
             <label
               htmlFor="realName"
-              className="block text-white font-semibold pb-2"
+              className="block text-[#00CDFF] font-bold pb-2"
             >
               Real Name
             </label>
@@ -117,13 +127,22 @@ function App() {
               value={formData.realName}
               onChange={handleChange}
               placeholder="Rishika or Shreyas? Uhh!"
-              className="w-full mt-1 p-2 border rounded-lg"
+              className={`w-full p-2 mt-1 border rounded-lg focus:outline-none focus:ring-2 ${
+                formErrors.realName
+                  ? "border-red-500 focus:ring-red-400"
+                  : "focus:ring-blue-400"
+              }`}
             />
+            {formErrors.realName && (
+              <p className="text-[#00CDFF] text-base mt-1">
+                {formErrors.realName}
+              </p>
+            )}
           </div>
           <div className="mb-4">
             <label
               htmlFor="email"
-              className="block text-white font-semibold pb-2"
+              className="block text-[#00CDFF] font-bold pb-2"
             >
               Email
             </label>
@@ -134,11 +153,20 @@ function App() {
               value={formData.email}
               onChange={handleChange}
               placeholder="Don't Worry! Not Selling it to Facebook"
-              className="w-full mt-1 p-2 border rounded-lg"
+              className={`w-full p-2 mt-1 border rounded-lg focus:outline-none focus:ring-2 ${
+                formErrors.email
+                  ? "border-red-500 focus:ring-red-400"
+                  : "focus:ring-blue-400"
+              }`}
             />
+            {formErrors.email && (
+              <p className="text-[#00CDFF] text-base mt-1">
+                {formErrors.email}
+              </p>
+            )}
           </div>
           <div className="mb-4">
-            <label className="block text-white font-semibold pb-2">
+            <label className="block text-[#00CDFF] font-semibold pb-2">
               Superpowers
             </label>
             <div className="flex flex-wrap gap-2 flex-col">
@@ -151,14 +179,14 @@ function App() {
               ].map((power) => (
                 <label
                   key={power}
-                  className="ms-2 text-base font-medium text-white dark:text-white"
+                  className="flex items-center text-base font-medium text-white dark:text-white gap-2"
                 >
                   <input
                     type="checkbox"
                     value={power}
                     checked={formData.superpowers.includes(power)}
                     onChange={handleCheckboxChange}
-                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                    className="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded-full focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                   />
                   {power}
                 </label>
@@ -168,7 +196,7 @@ function App() {
           <div className="mb-4">
             <label
               htmlFor="weakness"
-              className="block text-white font-semibold pb-2"
+              className="block text-[#00CDFF] font-semibold pb-2"
             >
               Weakness
             </label>
@@ -191,7 +219,7 @@ function App() {
           <div className="mb-4">
             <label
               htmlFor="profilePicture"
-              className="block text-white font-semibold pb-2"
+              className="block text-[#00CDFF] font-semibold pb-2"
             >
               Profile Picture
             </label>
@@ -203,19 +231,31 @@ function App() {
               onChange={handleFileChange}
               className="w-full p-2 mt-1 border rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400"
             />
+            {preview && (
+              <div className="mt-4">
+                <p className="text-[#00CDFF] font-semibold mb-2">
+                  Image Preview :{" "}
+                </p>
+                <img
+                  src={preview}
+                  alt="Profile Preview"
+                  className="w-32 h-32 rounded-full object-cover border-2 border-blue-500"
+                />
+              </div>
+            )}
           </div>
 
           <div className="text-center">
             <button
               type="submit"
-              className="bg-blue-600 text-white py-2 px-4 rounded-xl hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400"
+              className="w-full bg-[#00CDFF] text-white font-medium py-2 px-4 rounded-xl hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400"
             >
               Submit
             </button>
           </div>
         </form>
       </div>
-    </>
+    </div>
   );
 }
 
